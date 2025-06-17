f906549fd76cdf79d5307072c1386c42d9b5546b
Jack-Pision <jackpishon@gmail.com>
2025-06-17 20:35:39 +0600

    Use Inter font globally: apply next/font and remove Roboto import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className={`${inter.className} h-full`}>
       <body className="h-full antialiased">
         <Toaster
           position="bottom-right"
